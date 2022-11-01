d8f60e15ed5a5139d93e92a1d65286d75e00571b
Guilherme Albino <guilhermealbino33@gmail.com>
2022-11-01 16:58:14 -0300

    test: ensure SingUpController calls emailValidator with correct email

--- src/presentation/controllers/singup.spec.ts
+++ src/presentation/controllers/singup.spec.ts
@@ -88,7 +88,7 @@ describe("SingUp Controller", () => {
     );
   });
 
-  test("Should return 400 if an invalid e-mail is provided", () => {
+  test("Should return 400 if an invalid email is provided", () => {
     const { sut, emailValidatorStub } = makeSut();
     jest.spyOn(emailValidatorStub, "isValid").mockReturnValueOnce(false);
 
@@ -104,5 +104,22 @@ describe("SingUp Controller", () => {
 
     expect(httpResponse.statusCode).toBe(400);
     expect(httpResponse.body).toEqual(new InvalidParamError("email"));
+  });
+
+  test("Should call emailValidator with correct email", () => {
+    const { sut, emailValidatorStub } = makeSut();
+    const isValidSpy = jest.spyOn(emailValidatorStub, "isValid");
+
+    const httpRequest = {
+      body: {
+        name: "any_name",
+        email: "any_email@mail.com",
+        password: "any_password",
+        passwordConfirmation: "any_password",
+      },
+    };
+
+    sut.handle(httpRequest);
+    expect(isValidSpy).toHaveBeenCalledWith("any_email@mail.com");
   });
 });
