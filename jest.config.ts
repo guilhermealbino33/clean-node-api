740571d6079809aef2e55f94aceff129681556df
Guilherme Albino <guilhermealbino33@gmail.com>
2022-11-01 13:36:52 -0300

    fix: jest config

--- jest.config.ts
+++ jest.config.ts
@@ -1,8 +1,5 @@
 export default {
   roots: ["<rootDir>/src/"],
-  clearMocks: true, // nao tem no dele
-  collectCoverage: true, // nao tem no dele
-  coverageProvider: "v8", // nao tem no dele
   collectCoverageFrom: ["<rootDir>/src/**/*.ts"],
   coverageDirectory: "coverage",
   testEnvironment: "node",
