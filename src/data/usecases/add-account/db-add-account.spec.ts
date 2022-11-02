11177154fa17f1a132f01e7bf3c0d0c8f69a450b
Guilherme Albino <guilhermealbino33@gmail.com>
2022-11-02 15:15:08 -0300

    refactor: group all protocols in a single file

--- src/data/usecases/add-account/db-add-account.spec.ts
+++ src/data/usecases/add-account/db-add-account.spec.ts
@@ -1,5 +1,5 @@
 /* eslint-disable no-promise-executor-return */
-import { Encrypter } from "../../protocols/encrypter";
+import { Encrypter } from "./db-add-account-protocols";
 import { DbAddAccount } from "./db-add-account";
 
 const makeEncrypter = (): Encrypter => {
