b751cefda703e9265a185967dbf3ed891484b1a8
Guilherme Albino <guilhermealbino33@gmail.com>
2022-11-03 16:21:24 -0300

    feat: connect to mongodb before app listen

--- src/main/routes/singup.routes.test.ts
+++ src/main/routes/singup.routes.test.ts
@@ -2,9 +2,10 @@
 import request from "supertest";
 import { MongoHelper } from "../../infra/db/mongodb/helpers/mongo-helper";
 import app from "../config/app";
+import env from "../config/env";
 
 beforeAll(async () => {
-  await MongoHelper.connect(process.env.MONGO_URL);
+  await MongoHelper.connect(env.mongoUrl);
 });
 
 afterAll(async () => {
