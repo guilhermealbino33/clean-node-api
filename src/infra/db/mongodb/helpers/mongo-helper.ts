6cd0c435b14f835fd95e83de1d2096e5d9e84f4e
Guilherme Albino <guilhermealbino33@gmail.com>
2022-11-04 09:20:52 -0300

    feat: ensure client connects to mongodb if connection is down

--- src/infra/db/mongodb/helpers/mongo-helper.ts
+++ src/infra/db/mongodb/helpers/mongo-helper.ts
@@ -2,15 +2,22 @@ import { Collection, MongoClient } from "mongodb";
 
 export const MongoHelper = {
   client: null as MongoClient,
+  uri: null as string,
   async connect(uri: string): Promise<void> {
     this.client = await MongoClient.connect(uri);
+    this.uri = uri;
   },
 
   async disconnect(): Promise<void> {
     this.client.close();
+    this.client = null;
   },
 
   async getCollection(name: string): Promise<Collection> {
+    if (!this.client?.topology.isConnected()) {
+      await this.connect(this.uri);
+    }
+
     return this.client.db().collection(name);
   },
 
