29a6b3ed6a4634ab190a6950028fc0126e6ff58a
Guilherme Albino <guilhermealbino33@gmail.com>
2022-11-04 10:10:24 -0300

    feat: ensure LogControllerDecorator calss controler with correct values

--- src/main/factories/singup.ts
+++ src/main/factories/singup.ts
@@ -2,14 +2,21 @@ import { DbAddAccount } from "../../data/usecases/add-account/db-add-account";
 import { BcryptAdapter } from "../../infra/cryptography/bcrypt-adapter";
 import { AccountMongoRepository } from "../../infra/db/mongodb/account-repository/account";
 import { SingUpController } from "../../presentation/controllers/singup/singup";
+import { Controller } from "../../presentation/protocols";
 import { EmailValidatorAdapter } from "../../utils/email-validator-adapter";
+import { LogControllerDecorator } from "../decorators/log";
 
-export const makeSingUpController = (): SingUpController => {
+export const makeSingUpController = (): Controller => {
   const salt = 12;
   const emailValidatorAdapter = new EmailValidatorAdapter();
   const accountMongoRepository = new AccountMongoRepository();
   const bcryptAdapter = new BcryptAdapter(salt);
   const dbAddAccount = new DbAddAccount(bcryptAdapter, accountMongoRepository);
 
-  return new SingUpController(emailValidatorAdapter, dbAddAccount);
+  const singUpController = new SingUpController(
+    emailValidatorAdapter,
+    dbAddAccount
+  );
+
+  return new LogControllerDecorator(singUpController);
 };
