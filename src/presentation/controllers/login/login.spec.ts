a028056013d0369bdcfc065d7bf7456676708a42
Guilherme Albino <guilhermealbino33@gmail.com>
2022-11-10 14:19:37 -0300

    test: ensure LoginController calls EmailValidator with correct email

--- src/presentation/controllers/login/login.spec.ts
+++ src/presentation/controllers/login/login.spec.ts
@@ -1,15 +1,30 @@
 import { MissingParamError } from "../../errors";
 import { badRequest } from "../../helpers/http-helper";
+import { EmailValidator } from "../singup/singup-protocols";
 import { LoginController } from "./login";
+
+const makeEmailValidator = (): EmailValidator => {
+  class EmailValidatorStub implements EmailValidator {
+    isValid(email: string): boolean {
+      return true;
+    }
+  }
+  return new EmailValidatorStub();
+};
 
 interface SutTypes {
   sut: LoginController;
+  makeEmailValidatorStub: EmailValidator;
 }
 
 const makeSut = (): SutTypes => {
-  const sut = new LoginController();
+  const makeEmailValidatorStub = makeEmailValidator();
+  const sut = new LoginController(makeEmailValidatorStub);
 
-  return { sut };
+  return {
+    sut,
+    makeEmailValidatorStub,
+  };
 };
 
 describe("Login Controller", () => {
@@ -25,6 +40,7 @@ describe("Login Controller", () => {
 
     expect(httpResponse).toEqual(badRequest(new MissingParamError("email")));
   });
+
   test("Should return 400 if no password is provided", async () => {
     const { sut } = makeSut();
     const httpRequest = {
@@ -36,5 +52,20 @@ describe("Login Controller", () => {
     const httpResponse = await sut.handle(httpRequest);
 
     expect(httpResponse).toEqual(badRequest(new MissingParamError("password")));
+  });
+
+  test("Should call EmailValidator with correct email", async () => {
+    const { sut, makeEmailValidatorStub } = makeSut();
+    const isValidSpy = jest.spyOn(makeEmailValidatorStub, "isValid");
+
+    const httpRequest = {
+      body: {
+        email: "any_email@email.com",
+        password: "any_password",
+      },
+    };
+
+    sut.handle(httpRequest);
+    expect(isValidSpy).toHaveBeenCalledWith("any_email@email.com");
   });
 });
