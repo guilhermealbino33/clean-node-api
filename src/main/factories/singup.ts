b252b4f87b5ddaf377f5655f6e2f512a1ce3698f
Guilherme Albino <guilhermealbino33@gmail.com>
2022-11-10 11:06:07 -0300

    feat: add LogMongoRepository to singup factory

--- src/main/factories/singup.ts
+++ src/main/factories/singup.ts
@@ -1,6 +1,7 @@
 import { DbAddAccount } from "../../data/usecases/add-account/db-add-account";
 import { BcryptAdapter } from "../../infra/cryptography/bcrypt-adapter";
 import { AccountMongoRepository } from "../../infra/db/mongodb/account-repository/account";
+import { LogMongoRepository } from "../../infra/db/mongodb/log-repository/log";
 import { SingUpController } from "../../presentation/controllers/singup/singup";
 import { Controller } from "../../presentation/protocols";
 import { EmailValidatorAdapter } from "../../utils/email-validator-adapter";
@@ -18,5 +19,7 @@ export const makeSingUpController = (): Controller => {
     dbAddAccount
   );
 
-  return new LogControllerDecorator(singUpController);
+  const logMongoRepository = new LogMongoRepository();
+
+  return new LogControllerDecorator(singUpController, logMongoRepository);
 };
