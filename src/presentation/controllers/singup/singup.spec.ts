bedc48e8edbc116cc900fa65b99ca0f828c23ae2
Guilherme Albino <guilhermealbino33@gmail.com>
2022-11-11 14:47:56 -0300

    test: ensure SingUpController returns 400 if Validation fails

--- src/presentation/controllers/singup/singup.spec.ts
+++ src/presentation/controllers/singup/singup.spec.ts
@@ -240,4 +240,16 @@ describe("SingUp Controller", () => {
 
     expect(validateSpy).toHaveBeenCalledWith(httpRequest.body);
   });
+
+  test("Should return 400 if Validation return an error", async () => {
+    const { sut, validationStub } = makeSut();
+    jest
+      .spyOn(validationStub, "validate")
+      .mockReturnValueOnce(new MissingParamError("any_field"));
+
+    const httpResponse = await sut.handle(makeFakeRequest());
+    expect(httpResponse).toEqual(
+      badRequest(new MissingParamError("any_field"))
+    );
+  });
 });
