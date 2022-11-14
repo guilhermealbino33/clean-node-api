efc2d7dc6b903b686637e88e36cf28b186421417
Guilherme Albino <guilhermealbino33@gmail.com>
2022-11-14 09:39:40 -0300

    fix: field validation method

--- src/main/adapters/express-routes-adapter.ts
+++ src/main/adapters/express-routes-adapter.ts
@@ -6,13 +6,13 @@ export const adaptRoute = (controller: Controller) => {
     const httpRequest: HttpRequest = {
       body: req.body,
     };
+
     const httpResponse = await controller.handle(httpRequest);
-    if (httpResponse.statusCode === 200) {
-      res.status(httpResponse.statusCode).json(httpResponse.body);
+    const { statusCode, body } = httpResponse;
+    if (statusCode === 200) {
+      res.status(statusCode).json(body);
     } else {
-      res
-        .status(httpResponse.statusCode)
-        .json({ error: httpResponse.body.message });
+      res.status(statusCode).json({ error: body.message });
     }
   };
 };
