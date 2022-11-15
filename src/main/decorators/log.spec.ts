dc338d8d64ff376383bf35448262014128d44a2f
Guilherme Albino <guilhermealbino33@gmail.com>
2022-11-15 09:50:34 -0300

    refactor: move data layer interface to folders

--- src/main/decorators/log.spec.ts
+++ src/main/decorators/log.spec.ts
@@ -2,7 +2,7 @@
 /* eslint-disable max-classes-per-file */
 /* eslint-disable no-promise-executor-return */
 /* eslint-disable @typescript-eslint/no-unused-vars */
-import { LogErrorRepository } from "../../data/protocols/log-error-repository";
+import { LogErrorRepository } from "../../data/protocols/db/log-error-repository";
 import { AccountModel } from "../../domain/models/account";
 import { ok, serverError } from "../../presentation/helpers/http/http-helper";
 import {
