535e1f3bb01b15648057c7e5d4fadc084004e84b
Guilherme Albino <guilhermealbino33@gmail.com>
2023-05-20 20:47:59 -0300

    test: login route return 401 with invalid credentials

--- src/main/routes/login.routes.test.ts
+++ src/main/routes/login.routes.test.ts
@@ -1,7 +1,11 @@
 /* eslint-disable import/no-extraneous-dependencies */
 import request from "supertest";
+import { Collection } from "mongodb";
+import { hash } from "bcrypt";
 import { MongoHelper } from "../../infra/db/mongodb/helpers/mongo-helper";
 import app from "../config/app";
+
+let accountCollection: Collection;
 
 describe("Login Routes", () => {
   beforeAll(async () => {
@@ -13,7 +17,7 @@ describe("Login Routes", () => {
   });
 
   beforeEach(async () => {
-    const accountCollection = await MongoHelper.getCollection("accounts");
+    accountCollection = await MongoHelper.getCollection("accounts");
     accountCollection.deleteMany({});
   });
 
@@ -28,6 +32,42 @@ describe("Login Routes", () => {
           passwordConfirmation: "any_password",
         })
         .expect(200);
+    });
+  });
+
+  describe("POST /login", () => {
+    beforeEach(async () => {
+      accountCollection.deleteMany({});
+    });
+
+    test("Should return 200 on login", async () => {
+      const password = await hash("any_password", 12);
+
+      await accountCollection.insertOne({
+        name: "any_name",
+        email: "any_email@email.com",
+        password,
+      });
+
+      await request(app)
+        .post("/api/login")
+        .send({
+          name: "any_name",
+          email: "any_email@email.com",
+          password: "any_password",
+        })
+        .expect(200);
+    });
+
+    test("Should return 401 on login", async () => {
+      await request(app)
+        .post("/api/login")
+        .send({
+          name: "any_name",
+          email: "any_email@email.com",
+          password: "any_password",
+        })
+        .expect(401);
     });
   });
 });
